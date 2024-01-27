f29319892b271705f8690d66fc760d33f57c3d42
fatma nasser <128849238+fatmaelhot@users.noreply.github.com>
2024-01-27 11:14:30 +0200

    Update todolist.component.ts

--- todolist/src/app/todolist/todolist.component.ts
+++ todolist/src/app/todolist/todolist.component.ts
@@ -9,15 +9,9 @@ import { Component,Input } from '@angular/core';
 })
 export class TodolistComponent {
   @Input() InputFromParent: string[] = [];
-  // toggling attribute
-  status:boolean  = false;
 
   deleteTodo(index : number) {
     this.InputFromParent.splice(index, 1);
   }
-   // toggle status to show and hide line-through property (defined in list.component.css)
-   toggle():void
-   {
-      this.status = ! this.status ;
-   }
+   
 }
